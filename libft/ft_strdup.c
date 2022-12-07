3d51efafedff078d19157cc12b0fef1e346ca36b
nlibano42 <93001303+nlibano42@users.noreply.github.com>
2022-12-07 18:57:10 +0100

    Update ft_strdup.c

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -12,21 +12,6 @@
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
-{
-	size_t	i;
-
-	if (!dst && !src)
-		return (NULL);
-	i = 0;
-	while (i < n)
-	{
-		((char *)dst)[i] = ((char *)src)[i];
-		i++;
-	}
-	return (dst);
-}
-
 char	*ft_strdup(const char *s1)
 {
 	char	*dst;
